df1e585958dbb90e447ce235f9d9f3c21c25745f
Clif Jordan <cjordan@indelible.tv>
2012-12-03 13:40:28 -0500

    rename only the most current filter in the menu bar

--- filter.js
+++ filter.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
+    // always show these elements in filter results
+    function nonUCG(){
+        $('#site-content .content-box.poll').fadeIn('slow').removeClass('hidden');
+        $('#site-content .content-box.question').fadeIn('slow').removeClass('hidden');
+        $('#site-content .content-box .qblog').parent().fadeIn('slow').removeClass('hidden');
+    };
+    // reset the category names to there original state
+    function resetFilterHeadings(){
+            var categoryOne = $('.level-2').eq(0).attr('class').split(' ')[1];
+            $('.level-2.fitness > a').text(categoryOne);
+            var categoryTwo = $('.level-2').eq(1).attr('class').split(' ')[1];
+            $('.level-2.nutrition > a').text(categoryTwo);
+            var categoryThree = $('.level-2').eq(2).attr('class').split(' ')[1];
+            $('.level-2.lifestyle > a').text(categoryThree);
+            var categoryFour = $('.level-2').eq(3).attr('class').split(' ')[1];
+            $('.level-2.club > a').text(categoryFour);
+    };
+    // filter member like me
     $('.similar').click(function(){
+        $('.level-3.close').animate({opacity: 1}, 500);
         arrayFilterOption = [];
-
         var lifestyleoption = 'filteroption_' + $('body').data('lifestyle');
         var nutritionoption = 'filteroption_' + $('body').data('nutrition');
         var fitnessoption   = 'filteroption_' + $('body').data('fitness');
@@ -12,17 +30,21 @@ $(document).ready(function() {
                 $(this).removeClass('hidden').fadeIn('slow');
             }
         });
-        // display nonUGC elements polls question and qblogs
-        $('#site-content .content-box.poll').fadeIn('slow').removeClass('hidden');
-        $('#site-content .content-box.question').fadeIn('slow').removeClass('hidden');
-        $('#site-content .content-box .qblog').parent().fadeIn('slow').removeClass('hidden');
-        
+        // add results to flyout menu
+        var fitnessName = $(this).siblings('.caret').find('ul .level-2 ul li').find('.' + fitnessoption).eq(0).text();
+        $('.level-2.fitness > a').text(fitnessName);
+        var nutritionName = $(this).siblings('.caret').find('ul .level-2 ul li').find('.' + nutritionoption).eq(0).text();
+        $('.level-2.nutrition > a').text(nutritionName);
+        var lifestyleName = $(this).siblings('.caret').find('ul .level-2 ul li').find('.' + lifestyleoption).eq(0).text();
+        $('.level-2.lifestyle > a').text(lifestyleName);
+
+         nonUCG();       
         // re-run masonry
         $('#site-content').masonry();
-        //return false;
     });
-    
+    // filter randomly
     $('.random').click(function(){
+        $('.aux.close').animate({opacity: 1}, 500);
         arrayFilterOption = [];
         $('.caret .level-3 > a').each(function(){
             arrayFilterOption.push($(this).attr('class'));
@@ -38,21 +60,32 @@ $(document).ready(function() {
                 $(this).removeClass('hidden').fadeIn('slow');
             }
         });
+                
+        // change filter category headings to reflect the name selected
+        var filterCategoryName = $(this).siblings('.caret').find('ul .level-2 ul li').find('.' + filterVal).eq(0).text();
+        $(this).html('<span> | </span><a href="#">' + filterCategoryName + '</a>');
         
-        // display nonUGC elements polls question and qblogs
-        $('#site-content .content-box.poll').fadeIn('slow').removeClass('hidden');
-        $('#site-content .content-box.question').fadeIn('slow').removeClass('hidden');
-        $('#site-content .content-box .qblog').parent().fadeIn('slow').removeClass('hidden');
-        
+        nonUCG();
         // re-run masonry
         $('#site-content').masonry();
-        //return false;
     });
+    // reset the random filter
+    $('.aux.close').click(function(){
+        $(this).animate({opacity: 0}, 500);
+        // reset name back to random
+        $('.aux.random > a').text('SURPRISE ME');
+        // show all boxes
+        $(this).closest('#site-content-wrapper').find('#site-content .content-box.hidden').each(function(){
+            $(this).removeClass('hidden').fadeIn('slow')
+        });
+        $('#site-content').masonry();
+    }); 
     
-    // Flyout menu filter
+    // filter categories
     $('.level-3 a').click(function(){
         $(this).css('outline','none');
-        $('.close').animate({opacity: 1}, 500);
+        resetFilterHeadings();
+        $('.caret .close').animate({opacity: 1}, 500);
         $('.level-3.current').removeClass('current');
         $(this).parent().addClass('current');
         
@@ -69,17 +102,13 @@ $(document).ready(function() {
                 }
             });
         }
-        // display nonUGC elements polls question and qblogs
-        $('#site-content .content-box.poll').fadeIn('slow').removeClass('hidden');
-        $('#site-content .content-box.question').fadeIn('slow').removeClass('hidden');
-        $('#site-content .content-box .qblog').parent().fadeIn('slow').removeClass('hidden');
         // change filter category headings to reflect the name selected 
         var filterName = $(this).text();
         $(this).closest('ul').parent('.level-2').find('a').eq(0).text(filterName);
         
+        nonUCG();
         // re-run masonry
         $('#site-content').masonry();
-        //return false;
     });
     // change filter category headings to reflect the name selected
     $('.level-4 a').click(function(){
@@ -87,16 +116,8 @@ $(document).ready(function() {
         $(this).closest('nav').find('.club a').eq(0).text(filterName);
     });
     // controlling the close box
-    $('.close a').click(function(){
+    $('.close > a').click(function(){
         $('.level-3.close').animate({opacity: 0}, 500);
-            // reset the category names to there original state
-            var categoryOne = $('.level-2').eq(0).attr('class').split(' ')[1];
-            $('.level-2.fitness > a').text(categoryOne);
-            var categoryTwo = $('.level-2').eq(1).attr('class').split(' ')[1];
-            $('.level-2.nutrition > a').text(categoryTwo);
-            var categoryThree = $('.level-2').eq(2).attr('class').split(' ')[1];
-            $('.level-2.lifestyle > a').text(categoryThree);
-            var categoryFour = $('.level-2').eq(3).attr('class').split(' ')[1];
-            $('.level-2.club > a').text(categoryFour);
-    }); 
+            resetFilterHeadings();
+    });
 });
